eb25e508b2d8dd7bbade9da57a6d175944fca77a
yaswa <yaswagit@gmail.com>
2023-03-05 16:31:49 +0530

    loginSignUpLogout pages

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore } from 'vuex'
+import loginSignUpLogoutModule from './modules/loginSignUpLogout.module'
 
 export default createStore({
   state: {
+    loginSignUpLogoutState: loginSignUpLogoutModule.state
   },
   getters: {
+    getLoginSignUpLogoutState (state) {
+      return state.loginSignUpLogoutState.loginSignUpLogout
+    }
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+    loginSignUpLogoutModule
   }
 })
